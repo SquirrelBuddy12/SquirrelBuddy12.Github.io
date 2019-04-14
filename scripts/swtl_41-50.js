5770707eb296160ca776d610ca3974955563cec2
SquirrelBuddy12 <rebeccaleo17@gmail.com>
2019-04-14 09:34:35 -0400

    Added sites and small changes.

---
+++ scripts/swtl_41-50.js
@@ -0,0 +1,53 @@
+function chapter41() {
+  var i = 0;
+   while (i <= 2) {
+console.log("<p>Would you like a piece of pie?</p>");
+      document.write("<p>Would you like a piece of pie?</p>");
+	  i++
+	  
+ {
+ }
+ 
+function chapter42() {
+var daysOfWeek ";= new Array();<br>
+daysofWeek[0] = "Monday", daysOfWeek[1] = "Tuesday";<br>
+daysofWeek[2] = "Wednesday", daysOfWeek[3] = "Thursday";<br>
+daysofWeek[4] = "Friday", daysOfWeek[5] = "Saturday";<br>
+daysofWeek[6] = "Sunday";<br>
+var count = 0<br>
+do {<br>
+  document.write(daysOfWeek[count] + "<br  />");<br>
+  ++count;<br>
+} while (count &lt; daysOfWeek.length);<br>
+}
+
+function chapter43() {
+p id(p1, p2) {
+  return p1 / p2;
+}
+
+function chapter44() {
+// Look on the next few lines</p>
+/* This is a comment that
+*/
+
+function chapter45() {
+<a href="JavaScript:void(0)" onClick="alert('Good Morning')">Click Me</a>
+
+function chapter 46() {
+input type="button" value="order" onClick="alert('Please place your order')
+}
+
+function chapter47() {
+onMouseover="alert('Be sure to get your studying done today!');">School Ends in May</h1>
+}
+
+function chapter49() {
+function checkAddress(fieldId) {
+var val = document.getElementById(fieldId).value;
+if val === "") {
+alert("Email address required.");
+
+function chapter50() {
+  alert("Input field lost focus.");
+}
