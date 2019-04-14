c2fa068188ac9074452dec223f797ae44b7df84d
SquirrelBuddy12 <rebeccaleo17@gmail.com>
2019-04-14 09:55:02 -0400

    Move javascript into seperate file.

--- scripts/swtl_51-60.js
+++ scripts/swtl_51-60.js
@@ -1,10 +1,62 @@
-function bakery() {
-	var longParagraph = "We are glad you have arrived at Oodles of Yum. We have all kinds of cakes, cookies and pies to fulfill your sweetest desires.";
-	document.getElementById("concept").innerHTML = longParagraph;
+function chapter51() {
+var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine. primates which
+make up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety of
+distinctive coloration patterns that are species-dependent. The hands and feet of slow lorises have several
+adaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time.
+Slow lorises have a toxic bite, a rare trait among mammals.";
+document.getElementById("slowLoris").innerHTML = expandedParagraph;
+
+function peekAtContent() {
+   var whatsThere =
+document.getElementByID("content").innerHTML;
+}   
+
+function chapter52() {
+document.getElementById("p1").className += " big";
 }
-function makeInvisible() {
-	document.getElementById("me").className = "hidden";
+
+function chapter54() {
+  document.getElementById(eId).src = new.Pic;
+}  
+
+function chapter55() {
+document.getElementById("pic99").style.cssFloat = "left";
 }
-function swapPic(eId, newPic) {
-	document.getElementbyID(eId).src = newPic;
+
+function chapter56() {
+getElementById(the id).style.fontSize = "2
+var par = document.getElementsByTagName("p");
 }
+
+function chapter57() {
+var e = document.getElementByID("rules");
+var paragraphs = e.getElementsByTagName("p");
+var paragraphs = document.getElementsByTagName("P");
+// paragraphs[0] is the first <p> element
+// paragraphs[1] is the second <p> element
+alert(paragraphs[0].nodeName);
+All P elements must be returned.
+}
+
+function chapter59() {
+var x = document.getElementById("main");
+var y = x.getElementsByTagName("p");
+document.getElementById("demo").innerHTML = 
+'The first paragraph (index 0) inside "main" is: ' + y[0].innerHTML;
+<div id="div1">
+<p id="p1"This is a paragraph.</p>
+<p id="p2">This is another paragraph.</p>
+</div>
+var para = document.createElement("p");
+var node = document.createTextNode("This is new.");
+para.appendChild(node);
+var element = document.getElementById("div1");
+element.appendChild(para);
+</script>
+}
+
+function chapter60() {
+var d = document.getElementById("div3");
+var p = d.getElementsByTagName("p");
+var contents = p[2].innerHTML;
+	
